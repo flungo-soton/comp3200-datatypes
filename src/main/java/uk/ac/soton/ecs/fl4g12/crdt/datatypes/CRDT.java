3010db3f2778e641c38037f98bd89405b593f84b
Fabrizio Lungo <git@flungo.me>
2017-02-28 22:29:30 +0000

    Added CRDT interfaces
    Added the interfaces for CRDT (CmRDT and CvRDT) and created class diagrams as part of the visual paradigm
    project.

---
+++ src/main/java/uk/ac/soton/ecs/fl4g12/crdt/datatypes/CRDT.java
@@ -0,0 +1,41 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2016 Fabrizio Lungo <fl4g12@ecs.soton.ac.uk>.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package uk.ac.soton.ecs.fl4g12.crdt.datatypes;
+
+import uk.ac.soton.ecs.fl4g12.crdt.datatypes.commutative.CmRDT;
+import uk.ac.soton.ecs.fl4g12.crdt.datatypes.convergent.CvRDT;
+import uk.ac.soton.ecs.fl4g12.crdt.delivery.Updatable;
+import uk.ac.soton.ecs.fl4g12.crdt.delivery.UpdateMessage;
+
+/**
+ * Interface for Conflict-free Replicated Data Types. This interface should not be extended directly
+ * and the {@link CvRDT} and {@link CmRDT} should be used to determine the semantic of how the
+ * datatype works.
+ *
+ * @param <K> the type of identifier used to identify nodes.
+ * @param <U> the type of updates which this object can be updated by.
+ * @see CvRDT for the interface to be used with Convergent Replicated Data Types.
+ * @see CmRDT for the interface to be used with Commutative Replicated Data Types.
+ */
+public interface CRDT<K, U extends UpdateMessage<K>> extends Updatable<K, U> {
+
+}
---
+++ src/main/java/uk/ac/soton/ecs/fl4g12/crdt/datatypes/convergent/CvRDT.java
@@ -0,0 +1,41 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2016 Fabrizio Lungo <fl4g12@ecs.soton.ac.uk>.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package uk.ac.soton.ecs.fl4g12.crdt.datatypes.convergent;
+
+import uk.ac.soton.ecs.fl4g12.crdt.datatypes.CRDT;
+import uk.ac.soton.ecs.fl4g12.crdt.delivery.State;
+import uk.ac.soton.ecs.fl4g12.crdt.delivery.StateSnapshot;
+import uk.ac.soton.ecs.fl4g12.crdt.delivery.UpdateMessage;
+
+/**
+ * Interface for Convergent Replicated Data Types. {@linkplain CvRDT}s are based on {@link State}s
+ * and use {@link StateSnapshot}s to communicate changes made.
+ *
+ * @param <K> the type of identifier used to identify nodes.
+ * @param <T> the type of timestamps which are used by each node.
+ * @param <S> the type of the {@link StateSnapshot} which is taken and used as the
+ *        {@link UpdateMessage}.
+ */
+public interface CvRDT<K, T extends Comparable<T>, S extends StateSnapshot<K, T>>
+    extends CRDT<K, S>, State<K, T, S> {
+
+}
