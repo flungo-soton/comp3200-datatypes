408acd8f72f7b3059f66dae8241ac303fbdf1b3a
Fabrizio Lungo <git@flungo.me>
2017-03-07 20:59:25 +0000

    Implemented PNCounter
    Implemented the PNCounter that allows both increment and decrement with tests.

--- src/main/java/uk/ac/soton/ecs/fl4g12/crdt/datatypes/convergent/PNCounter.java
+++ src/main/java/uk/ac/soton/ecs/fl4g12/crdt/datatypes/convergent/PNCounter.java
@@ -23,14 +23,13 @@ package uk.ac.soton.ecs.fl4g12.crdt.datatypes.convergent;
 
 import org.openimaj.citation.annotation.Reference;
 import org.openimaj.citation.annotation.ReferenceType;
-import uk.ac.soton.ecs.fl4g12.crdt.datatypes.CRDT;
 import uk.ac.soton.ecs.fl4g12.crdt.datatypes.Counter;
 import uk.ac.soton.ecs.fl4g12.crdt.delivery.AbstractVersionedUpdatable;
 import uk.ac.soton.ecs.fl4g12.crdt.delivery.DeliveryChannel;
 import uk.ac.soton.ecs.fl4g12.crdt.delivery.DeliveryUpdateException;
-import uk.ac.soton.ecs.fl4g12.crdt.delivery.UpdateMessage;
 import uk.ac.soton.ecs.fl4g12.crdt.order.HashVersionVector;
 import uk.ac.soton.ecs.fl4g12.crdt.order.IntegerVersion;
+import uk.ac.soton.ecs.fl4g12.crdt.order.LocalVersionVector;
 import uk.ac.soton.ecs.fl4g12.crdt.order.LongVersion;
 import uk.ac.soton.ecs.fl4g12.crdt.order.VersionVector;
 import uk.ac.soton.ecs.fl4g12.crdt.util.Arithmetic;
@@ -38,8 +37,8 @@ import uk.ac.soton.ecs.fl4g12.crdt.util.IntegerArithmetic;
 import uk.ac.soton.ecs.fl4g12.crdt.util.LongArithmetic;
 
 /**
- * Grow only {@linkplain CRDT} {@linkplain Counter} using the version vector as the counter of
- * increments at each node.
+ * A {@linkplain CRDT} {@linkplain Counter} using a positive and negative vector that are used to
+ * count increments and decrements at each node.
  *
  * @param <E> the type of the counter value.
  * @param <K> the type of identifier used to identify nodes.
@@ -48,85 +47,94 @@ import uk.ac.soton.ecs.fl4g12.crdt.util.LongArithmetic;
     author = {"Shapiro, Marc", "Preguica, Nuno", "Baquero, Carlos", "Zawirski, Marek"},
     title = "A comprehensive study of Convergent and Commutative Replicated Data Types",
     institution = "inria", year = "2011", url = "https://hal.inria.fr/inria-00555588",
-    pages = {"14", "15"})
-public final class GCounter<E extends Comparable<E>, K>
-    extends AbstractVersionedUpdatable<K, E, GCounterState<E, K>>
-    implements CvRDT<K, E, GCounterState<E, K>>, Counter<E> {
+    pages = {"15", "16"})
+public final class PNCounter<E extends Comparable<E>, K>
+    extends AbstractVersionedUpdatable<K, E, PNCounterState<E, K>>
+    implements CvRDT<K, E, PNCounterState<E, K>>, Counter<E> {
 
   private final Arithmetic<E> arithmetic;
+  private final LocalVersionVector<K, E> p;
+  private final LocalVersionVector<K, E> n;
 
   /**
-   * Construct a grow only counter that uses its {@linkplain VersionVector} as the state.
+   * Construct a pn-counter that uses its two {@linkplain VersionVector}'s as state.
    *
-   * @param arithmetic an {@link Arithmetic} instance which can add values of the same type as the
-   *        counter.
-   * @param initialVersion the initial {@link VersionVector} value to use. This should be a zero
-   *        version for a new counter as the sum of the timestamps is used as the value of the
-   *        counter.
+   * @param arithmetic an {@link Arithmetic} instance which can add/subtract values of the same type
+   *        as the counter.
+   * @param initialVersion the initial {@link VersionVector} value to use for the {@code version},
+   *        {@code p} and {@code n}. This should be a zero version for a new counter as the sum of
+   *        the timestamps is used as the value of the counter.
    * @param identifier the identifier of this instance or null for it to be assigned by the
    *        {@link DeliveryChannel}.
    * @param deliveryChannel the {@link DeliveryChannel} which this object should communicate changes
    *        over.
    */
-  public GCounter(Arithmetic<E> arithmetic, VersionVector<K, E> initialVersion, K identifier,
-      DeliveryChannel<K, GCounterState<E, K>> deliveryChannel) {
+  public PNCounter(Arithmetic<E> arithmetic, VersionVector<K, E> initialVersion, K identifier,
+      DeliveryChannel<K, PNCounterState<E, K>> deliveryChannel) {
     super(initialVersion, identifier, deliveryChannel);
     this.arithmetic = arithmetic;
+    this.p = new LocalVersionVector<>(initialVersion.copy(), getIdentifier());
+    this.n = new LocalVersionVector<>(initialVersion.copy(), getIdentifier());
   }
 
   @Override
   public synchronized void increment() {
     version.increment();
+    p.increment();
     getDeliveryChannel().publish(snapshot());
   }
 
   @Override
-  public void decrement() {
-    throw new UnsupportedOperationException("Grow only counter can only be incremented.");
+  public synchronized void decrement() {
+    version.increment();
+    n.increment();
+    getDeliveryChannel().publish(snapshot());
   }
 
   @Override
   public E value() {
-    return arithmetic.add(version.get().values());
+    return arithmetic.sub(arithmetic.add(p.get().values()), n.get().values());
   }
 
   @Override
-  public synchronized void update(GCounterState<E, K> message) throws DeliveryUpdateException {
+  public synchronized void update(PNCounterState<E, K> message) throws DeliveryUpdateException {
     version.sync(message.getVersionVector());
+    p.sync(message.getP());
+    n.sync(message.getN());
   }
 
   @Override
-  public synchronized GCounterState<E, K> snapshot() {
-    return new GCounterState<>(getIdentifier(), version);
+  public synchronized PNCounterState<E, K> snapshot() {
+    return new PNCounterState<>(getIdentifier(), version, p, n);
   }
 
   /**
-   * Construct a new {@linkplain Integer} value {@linkplain GCounter}. It is expected that the
+   * Construct a new {@linkplain Integer} value {@linkplain PNCounter}. It is expected that the
    * {@link DeliveryChannel} provided will assign the instance its identifier.
    *
    * @param <K> the type of the identifiers used for instances.
    * @param deliveryChannel the {@link DeliveryChannel} which can be used to deliver
-   *        {@link UpdateMessage}s between instances of the same {@link GCounter}.
-   * @return a {@link GCounter} with an {@link Integer} value.
+   *        {@link UpdateMessage}s between instances of the same {@link PNCounter}.
+   * @return a {@link PNCounter} with an {@link Integer} value.
    */
-  public static <K> GCounter<Integer, K> newIntegerGCounter(
-      DeliveryChannel<K, GCounterState<Integer, K>> deliveryChannel) {
-    return new GCounter<>(IntegerArithmetic.getInstance(),
+  public static <K> PNCounter<Integer, K> newIntegerPNCounter(
+      DeliveryChannel<K, PNCounterState<Integer, K>> deliveryChannel) {
+    return new PNCounter<>(IntegerArithmetic.getInstance(),
         new HashVersionVector<K, Integer>(new IntegerVersion(), false), null, deliveryChannel);
   }
 
   /**
-   * Construct a new {@linkplain Long} value {@linkplain GCounter}. It is expected that the
+   * Construct a new {@linkplain Long} value {@linkplain PNCounter}. It is expected that the
    * {@link DeliveryChannel} provided will assign the instance its identifier.
    *
    * @param <K> the type of the identifiers used for instances.
    * @param deliveryChannel the {@link DeliveryChannel} which can be used to deliver
-   *        {@link UpdateMessage}s between instances of the same {@link GCounter}.
-   * @return a {@link GCounter} with an {@link Long} value.
+   *        {@link UpdateMessage}s between instances of the same {@link PNCounter}.
+   * @return a {@link PNCounter} with an {@link Long} value.
    */
-  public static <K> GCounter<Long, K> newLongGCounter(
-      DeliveryChannel<K, GCounterState<Long, K>> deliveryChannel) {
-    return new GCounter<>(LongArithmetic.getInstance(),
+  public static <K> PNCounter<Long, K> newLongPNCounter(
+      DeliveryChannel<K, PNCounterState<Long, K>> deliveryChannel) {
+    return new PNCounter<>(LongArithmetic.getInstance(),
         new HashVersionVector<K, Long>(new LongVersion(), false), null, deliveryChannel);
   }
 }
---
+++ src/main/java/uk/ac/soton/ecs/fl4g12/crdt/datatypes/convergent/PNCounterState.java
@@ -0,0 +1,71 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2017 Fabrizio Lungo <fl4g12@ecs.soton.ac.uk>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package uk.ac.soton.ecs.fl4g12.crdt.datatypes.convergent;
+
+import uk.ac.soton.ecs.fl4g12.crdt.delivery.AbstractVersionedUpdateMessage;
+import uk.ac.soton.ecs.fl4g12.crdt.delivery.StateSnapshot;
+import uk.ac.soton.ecs.fl4g12.crdt.order.VersionVector;
+
+/**
+ * Representation of the state for a {@linkplain PNCounter}.
+ *
+ * @param <E> the type of the counter value.
+ * @param <K> the type of identifier used to identify nodes.
+ */
+public final class PNCounterState<E extends Comparable<E>, K>
+    extends AbstractVersionedUpdateMessage<K, E> implements StateSnapshot<K, E> {
+
+  private final VersionVector<K, E> p;
+  private final VersionVector<K, E> n;
+
+  /**
+   * Instantiate a new {@linkplain GCounterState}.
+   *
+   * @param identifier the identifier of the instance that was updated.
+   * @param versionVector the version as a result of the update.
+   */
+  PNCounterState(K identifier, VersionVector<K, E> versionVector, VersionVector<K, E> p,
+      VersionVector<K, E> n) {
+    super(identifier, versionVector);
+    this.p = p.copy();
+    this.n = n.copy();
+  }
+
+  /**
+   * Get the positive counter {@linkplain VersionVector}.
+   *
+   * @return the positive counter {@link VersionVector}.
+   */
+  public VersionVector<K, E> getP() {
+    return p.copy();
+  }
+
+  /**
+   * Get the negative counter {@linkplain VersionVector}.
+   *
+   * @return the negative counter {@link VersionVector}.
+   */
+  public VersionVector<K, E> getN() {
+    return n.copy();
+  }
+
+}
